5dd6375524dae5125ea1ceba386c33ebe8af8d4d
Veronica Moran <veronicamoran25@outlook.com>
2025-10-13 22:10:56 -0600

    Subir app

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import CalculadoraScreen from './screens/CalculadoraScreen';
+import { Layout } from './components';
+import {Button} from 'react-native-web';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <CalculadoraScreen/>
-    </View>
+    <Layout title="Contacto">
+      <Button 
+      title='Agregar'
+      color="#c88383ff" />
+    </Layout>
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+//const styles = StyleSheet.create({
+  //container: {
+   // flex: 1,
+    //backgroundColor: '#fff',
+    //alignItems: 'center',
+    //justifyContent: 'center',
+  //},
+//});
