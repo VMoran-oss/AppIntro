09a31ed19ab0e895e9161bf0170a840620da2fcc
Veronica Moran <veronicamoran25@outlook.com>
2025-10-14 17:20:51 -0600

    Subir input component

--- App.js
+++ App.js
@@ -1,25 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import CalculadoraScreen from './screens/CalculadoraScreen';
-import { Layout } from './components';
-import {Button} from 'react-native-web';
+import { Layout, Input } from './components';
+import { Button } from 'react-native-web';
 
 export default function App() {
   return (
     <Layout title="Contacto">
-      <Button 
-      title='Agregar'
-      color="#c88383ff" />
+      <Input label="Email:" placeholder="name@mail.com" type="email-addres" />
+      <Input label="Asunto:" placeholder="Consulta" />
+      <Input label="Mensaje:" placeholder="Escribir aqui..." lines={4} />
+       <Button
+        title='Agregar'
+        color="#c88383ff" />
     </Layout>
 
   );
 }
 
 //const styles = StyleSheet.create({
-  //container: {
-   // flex: 1,
-    //backgroundColor: '#fff',
-    //alignItems: 'center',
-    //justifyContent: 'center',
-  //},
+//container: {
+// flex: 1,
+//backgroundColor: '#fff',
+//alignItems: 'center',
+//justifyContent: 'center',
+//},
 //});
